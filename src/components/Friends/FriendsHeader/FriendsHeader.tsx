9275d1adad39dd6bc01a6a98c7742983e93365b0
volokhinn <csgomx@mail.ru>
2024-04-06 11:49:44 +0700

    fix friendsHeader

--- src/components/Friends/FriendsHeader/FriendsHeader.tsx
+++ src/components/Friends/FriendsHeader/FriendsHeader.tsx
@@ -22,7 +22,7 @@ const FriendsHeader = () => {
       <div className={styles.link}>Мои друзья</div>
       <Link to="/history" className={styles.link}>История</Link>
       {isAddFriendPage ? (
-        <IconButton disabled sx={{backgroundColor: 'rgba(229, 47, 91, 1)', color: '#fff', border: '1px solid rgba(229, 47, 91, 1)', transition: '.3s', "&:hover": { color: "rgba(229, 47, 91, 1)", backgroundColor: '#fff' } }}>
+        <IconButton sx={{backgroundColor: 'rgba(229, 47, 91, 1)', color: '#fff', border: '1px solid rgba(229, 47, 91, 1)', pointerEvents: 'none' }}>
             <AddOutlinedIcon />
         </IconButton>
       ) : (
