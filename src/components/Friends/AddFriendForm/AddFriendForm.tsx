64c41b15a48135615fce00beef27486f888641f3
volokhinn <csgomx@mail.ru>
2024-04-19 15:22:54 +0700

    Added uploading avatar for friend

--- src/components/Friends/AddFriendForm/AddFriendForm.tsx
+++ src/components/Friends/AddFriendForm/AddFriendForm.tsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import { TextField, Alert } from '@mui/material';
-import Button from '../../UI/Button/Button';
+import {Button as ButtonMUI} from '@mui/material';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import styles from './AddFriendForm.module.scss';
 import Snack from '../../UI/Snack/Snack';
+import Button from '../../UI/Button/Button';
 
 import { supabaseClient } from '../../../supabase';
 import { useAuth } from '@clerk/clerk-react';
 
 const AddFriendForm = () => {
   const [name, setName] = useState('');
-  const [img, setImg] = useState('');
+  const [file, setfile] = useState<File | null>(null);
+  
   const [showError, setShowError] = useState(false);
   const [openSnackBar, setOpenSnackBar] = useState(false);
 
+  const BUCKET_URL="https://nigothnmpjcxraitwgte.supabase.co/storage/v1/object/public"
+  
   const { getToken } = useAuth();
 
   const handleAddFriend = async () => {
@@ -23,26 +28,51 @@ const AddFriendForm = () => {
   
     try {
       const supabaseAccessToken = await getToken({ template: 'supabase' });
-
       const supabase = await supabaseClient(supabaseAccessToken);
-      console.log(supabaseAccessToken);
-      setShowError(false);
-      const { error } = await supabase.from('friends').insert([
-        { name: name[0].toUpperCase() + name.slice(1), img, money: 0 }
-      ]);
+
+      if (file) {
+        const filename = `${Date.now()}_${file.name}`
+      const { data } = await supabase.storage.from('avatars').upload(`/avatars/${filename}`, file, {
+        upsert: true,
+        contentType: 'image/*'
+      });
       
+
+      const filepath = data?.path;
+
+      const { error } = await supabase.from('friends').insert([
+        { name: name[0].toUpperCase() + name.slice(1), img: `${BUCKET_URL}/avatars/${filepath}`, money: 0 }
+      ]);
+  
       if (error) {
         console.error('Error adding friend:', error.message);
         return;
       }
-      
+
+      } else {
+        const { error } = await supabase.from('friends').insert([
+          { name: name[0].toUpperCase() + name.slice(1), img: '', money: 0 }
+        ]);
+
+        if (error) {
+          console.error('Error adding friend:', error.message);
+          return;
+        }
+      }
+
       setName('');
-      setImg('');
+      setfile(null);
       setOpenSnackBar(true);
     } catch (error) {
-      console.error('Error adding friend:');
+      console.error('Error adding friend:', error);
     }
-  };  
+  };
+
+  const handleFileSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setfile(e.target.files[0]);
+    }
+  };
 
   return (
     <div className={styles.main}>
@@ -53,12 +83,22 @@ const AddFriendForm = () => {
         sx={{ width: '100%' }}
       />
       {showError && <Alert severity="error">Длина имени должна быть от 2 до 10 символов</Alert>}
-      <TextField
-        value={img}
-        onChange={(e) => setImg(e.target.value)}
-        label="Ссылка на фото (необязательно)"
-        sx={{ width: '100%' }}
-      />
+        <ButtonMUI
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<CloudUploadIcon />}
+        >
+          {file ? file.name : 'Добавить фото'}
+          <input
+            type="file"
+            name='image'
+            accept="image/jpeg, image/png"
+            hidden
+            onChange={handleFileSelected}
+          />
+        </ButtonMUI>
       <div className={styles.btn}>
         <Button buttontext="Добавить" onClick={handleAddFriend} />
       </div>
